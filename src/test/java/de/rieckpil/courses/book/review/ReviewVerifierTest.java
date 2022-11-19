45047479708430da94a262b4fb0b3057a5972380
Eric Opoku <eopoku@eopoku01.mynetworksettings.com>
2022-11-19 08:02:30 -0500

    add tests for ReviewVerifier

--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -1,21 +1,11 @@
 package de.rieckpil.courses.book.review;
 
-import org.assertj.core.api.Assertions;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import java.util.List;
-
 import static de.rieckpil.courses.book.review.RandomReviewParameterResolverExtension.RandomReview;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(RandomReviewParameterResolverExtension.class)
 class ReviewVerifierTest {
@@ -23,42 +13,50 @@ class ReviewVerifierTest {
   private ReviewVerifier reviewVerifier;
 
   @BeforeEach
-  void setup() {
+  public void setUp() {
     reviewVerifier = new ReviewVerifier();
   }
 
   @Test
   void shouldFailWhenReviewContainsSwearWord() {
-    String review = "This book is shit";
-    System.out.println("Testing a review");
 
-    boolean result = reviewVerifier.doesMeetQualityStandards(review);
-    assertFalse(result, "ReviewVerifier did not detect swear word");
+    var review = "This book is shit";
+
+    var result = reviewVerifier.doesMeetQualityStandards(review);
+    Assertions.assertFalse(result, "ReviewVerifier did not detect sear word");
   }
 
   @Test
   @DisplayName("Should fail when review contains 'lorem ipsum'")
   void testLoremIpsum() {
+    String review = "Lorem Ipsum generated text...";
+
+    var result = reviewVerifier.doesMeetQualityStandards(review);
+    Assertions.assertFalse(result, "ReviewVerifier did not detect Lorem Ipsum");
   }
 
   @ParameterizedTest
   @CsvFileSource(resources = "/badReview.csv")
   void shouldFailWhenReviewIsOfBadQuality(String review) {
+
+    var result = reviewVerifier.doesMeetQualityStandards(review);
+    Assertions.assertFalse(result, "ReviewVerifier did not detect bad review");
+
   }
 
   @RepeatedTest(5)
   void shouldFailWhenRandomReviewQualityIsBad(@RandomReview String review) {
+    var result = reviewVerifier.doesMeetQualityStandards(review);
+    Assertions.assertFalse(result, "ReviewVerifier did not detect random review");
   }
 
   @Test
   void shouldPassWhenReviewIsGood() {
+    var review = "I can totally recommend this book " +
+      "who is interested in learning hwo to write Java code!";
+
+    var result = reviewVerifier.doesMeetQualityStandards(review);
+    Assertions.assertTrue(result, "ReviewVerifier did not pass a good review");
   }
 
-  @Test
-  void shouldPassWhenReviewIsGoodHamcrest() {
-  }
-
-  @Test
-  void shouldPassWhenReviewIsGoodAssertJ() {
-  }
 }
